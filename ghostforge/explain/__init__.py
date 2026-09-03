a033601b6bdbf7530d4642dda1e40f13753cd58d
amitee <amitkumar023410@gmail.com>
2026-09-03 16:03:02 +0530

    refactor: export attribution and attention helpers from explain init

--- ghostforge/explain/__init__.py
+++ ghostforge/explain/__init__.py
@@ -1,5 +1,15 @@
 """Explain package for evidence chains."""
 
-from ghostforge.explain.evidence import EvidenceChain, build_evidence
+from ghostforge.explain.attention import attention_to_contrib, edge_attention
+from ghostforge.explain.attribution import feature_attribution, top_features
+from ghostforge.explain.evidence import EvidenceChain, build_evidence, evidence_to_markdown
 
-__all__ = ["EvidenceChain", "build_evidence"]
+__all__ = [
+    "EvidenceChain",
+    "build_evidence",
+    "evidence_to_markdown",
+    "feature_attribution",
+    "top_features",
+    "edge_attention",
+    "attention_to_contrib",
+]
